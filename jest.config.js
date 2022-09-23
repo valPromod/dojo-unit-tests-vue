7523fefdc1c17207b0669d8cd4c80768f09ebd7f
Valentin Troadec <Valentin.Troadec-ext@promod.fr>
2022-09-23 10:04:02 +0200

    install + premiers TUs

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/typescript-and-babel",
+  globals: {
+    "ts-jest": {
+      diagnostics: false,
+    },
+  },
+  setupFiles: ["./tests/setup.ts"],
+  transformIgnorePatterns: [
+    "/node_modules/(?!vue)",
+    "/node_modules/@launcher/(?!vue-component-header|vue-component-datatable|utilities)",
+  ],
+  collectCoverage: false,
+  collectCoverageFrom: ["**/*.{ts,vue}", "!**/node_modules/**"],
 };
